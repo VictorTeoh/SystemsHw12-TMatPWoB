8ad79922ac619789a038777bc92898daefcf47cd
Piotr Milewski <pmilewski@stuy.edu>
2017-11-16 20:05:01 -0500

    Both children always finish now

--- forkin.c
+++ forkin.c
@@ -4,8 +4,8 @@ int childAct() {
   srand(getpid()); //seed RNG to not NULL. Otherwise same number will be generated.
   printf("Child PID: %d\n", getpid());
     
-  int sleepTime = 1;
-  //sleepTime = 5 + rand() % 16;
+  //int sleepTime = 3;
+  int sleepTime = 5 + rand() % 16;
   sleep(sleepTime);
   printf("Child is now woke\n");
     
@@ -14,17 +14,25 @@ int childAct() {
 
 int main(){
   
-  int cpid, status, wokeChild;
+  int ppid, cpid, cpid2, temp, status, wokeChild;
   printf("\nInitial Message\n");
   
+  ppid = getpid();
   cpid = fork(); //initial fork
   
   //Executables for Parent and Children
   if (cpid) { //if parent
-    cpid = fork();
-    if (cpid) {
-      wokeChild = wait(&status);
+    temp = cpid;
+    cpid = 0;
+    cpid2 = fork();
+    if (ppid == getpid()) {
+      cpid = temp; 
+      
+      wokeChild = waitpid(cpid, &status, 0);
       printf("Child %d finished after %d seconds\n", wokeChild, WEXITSTATUS(status));
+      
+      wokeChild = waitpid(cpid2, &status, 0);
+      printf("Child %d Finished after %d seconds\n", wokeChild, WEXITSTATUS(status));
       printf("Parent wants children to go back to bed\n");
     }
     else {
