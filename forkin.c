29af25395e86e8036a22914cbda3676b3665967e
Piotr Milewski <pmilewski@stuy.edu>
2017-11-16 20:19:19 -0500

    done

--- forkin.c
+++ forkin.c
@@ -7,32 +7,32 @@ int childAct() {
   //int sleepTime = 3;
   int sleepTime = 5 + rand() % 16;
   sleep(sleepTime);
-  printf("Child is now woke\n");
+  printf("Child %d is now woke\n", getpid());
     
   return sleepTime; 
 }
 
 int main(){
   
-  int ppid, cpid, cpid2, temp, status, wokeChild;
+  int ppid, cpid1, cpid2, temp, sts1, sts2;
   printf("\nInitial Message\n");
   
   ppid = getpid();
-  cpid = fork(); //initial fork
+  cpid1 = fork(); //initial fork
   
   //Executables for Parent and Children
-  if (cpid) { //if parent
-    temp = cpid;
-    cpid = 0;
+  if (cpid1) { //if parent
+    temp = cpid1;
+    cpid1 = 0;
     cpid2 = fork();
     if (ppid == getpid()) {
-      cpid = temp; 
+      cpid1 = temp; 
       
-      wokeChild = waitpid(cpid, &status, 0);
-      printf("Child %d finished after %d seconds\n", wokeChild, WEXITSTATUS(status));
-      
-      wokeChild = waitpid(cpid2, &status, 0);
-      printf("Child %d Finished after %d seconds\n", wokeChild, WEXITSTATUS(status));
+      waitpid(cpid1, &sts1, 0);
+      waitpid(cpid2, &sts2, 0);
+
+      printf("Child %d finished after %d seconds\n", cpid1, WEXITSTATUS(sts1)); 
+      printf("Child %d Finished after %d seconds\n", cpid2, WEXITSTATUS(sts2));
       printf("Parent wants children to go back to bed\n");
     }
     else {
